f52aaeef9605df244c92e64dccf79299cb9241e3
Victor Olsson <virre_olsson@hotmail.com>
2019-08-25 15:40:20 +0200

    Add files via upload

---
+++ precache-manifest.05c84c5ef56d267d779caad739241fad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b36b4fadb07cd6edbdef41ce938b801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "408635fbc5986c7f3231",
+    "url": "/static/js/2.ada4516e.chunk.js"
+  },
+  {
+    "revision": "13bb1fb51d71c4048b9a",
+    "url": "/static/js/main.ab6b42a9.chunk.js"
+  },
+  {
+    "revision": "57d799b15b2dda3c2074",
+    "url": "/static/js/runtime~main.c30e6f84.js"
+  },
+  {
+    "revision": "03cbc304ef94d2ad972d8dbba5e1ac57",
+    "url": "/static/media/gameBoard.03cbc304.png"
+  }
+]);
